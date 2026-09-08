688fcd7f98448825fcb79ecde33e9cf66c9925b5
joaovinicius123 <joaoviniciusalvesnascimento@gmail.com>
2026-09-08 12:10:23 -0300

    Adicionei a movimentação do player

---
+++ Assets/Scripts/movimentolateral.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class movimentolateral: MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float horizontal;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        transform.position += Vector3.right * horizontal * speed * Time.deltaTime;
+    }
+}

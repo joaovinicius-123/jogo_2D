70acaecf7d59dee88f2b53e3c2773b8ef92ec02c
Rodrigo Leonardo <joaoviniciusalvesnascimento@gmail.com>
2026-09-22 09:43:38 -0300

    Adicionei o  sistema de colisor no chão
    Foi adicionado quando o jogador estiver no chão e quando ele não estiver no chão

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private bool isGrounded = false;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,11 +22,29 @@ public class Player : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal"); //Vai reconhecer o movimento horizontal
 
-        rb.linearVelocity = new Vector2(moveHorizontal* speed, rb.linearVelocity.y); //Fará o objeto se movimentar para a direção do eixo x e y;
+        rb.linearVelocity = new Vector2(moveHorizontal * speed, rb.linearVelocity.y); //Fará o objeto se movimentar para a direção do eixo x e y;
 
-        if (Input.GetKeyDown(KeyCode.Space)) //Atribuirá o movimento de pulo na tecla espaço
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) //Atribuirá o movimento de pulo na tecla espaço
         {
             rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse); //Vai desenvolver a fisica necessaria para o objeto pular
         }
+    }
+
+     void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; //Vai reconhecer quando o jogador esta no chão.
+        }
+    }
+
+    void OnCollisionExit2D( Collision2D collision)
+
+    {   
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false; //vai reconhecer quando o jogador não estiver no chão
+        }
+      
     }
 }
